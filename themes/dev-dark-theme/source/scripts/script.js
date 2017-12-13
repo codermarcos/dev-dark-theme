c97f2b4f091f3956d187b62efe7380d98e7548b9
Marcos Carvalho <mcarvalho@allin.com.br>
2017-12-13 15:33:35 -0200

    adding animations

--- themes/dev-dark-theme/source/scripts/script.js
+++ themes/dev-dark-theme/source/scripts/script.js
@@ -1,21 +1,21 @@
 window.app = () => {
-  let pageY = 0;
+  let scrollY = 0;
   let header = document.getElementsByClassName('navbar')[0];
 
   window.onscroll = e => {
-    if (e.pageY === 0) {
+    if (window.scrollY === 0) {
       header.classList.add('clear');
       header.classList.remove('invisible');
     } else {
       header.classList.remove('clear');
-      if (e.pageY > pageY) {
+      if (window.scrollY > scrollY) {
         header.classList.add('invisible');
       } else {
         header.classList.remove('invisible');
       }
     }
 
-    pageY = e.pageY;
+    scrollY = window.scrollY;
   };
 };
 
