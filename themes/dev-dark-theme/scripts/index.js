ca7ff2e7b90680d131495556fcbe04c46ee64470
codermarcos <marcosbmx.jr@gmail.com>
2018-01-12 06:02:06 -0200

    adding to lowercase links

--- themes/dev-dark-theme/scripts/index.js
+++ themes/dev-dark-theme/scripts/index.js
@@ -3,14 +3,14 @@ hexo.extend.helper.register('url_categories', args => {
   let categories = [];
 
   original.data.forEach(element => {
-    categories.push(element.name.replace(new RegExp(' ', 'g'), '-'));
+    categories.push(element.name.replace(new RegExp(' ', 'g'), '-').toLowerCase());
   });
 
   return categories.join('/');
 });
 hexo.extend.helper.register('first_category', args => {
   let category = args.data[0] ? args.data[0].name : '';
-  return category.replace(new RegExp(' ', 'g'), '-');
+  return category.replace(new RegExp(' ', 'g'), '-').toLowerCase();
 });
 hexo.extend.helper.register('url_title', args => {
   let original = args;
