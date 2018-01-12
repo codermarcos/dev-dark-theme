800de45b6f2d818812ff9def9dff018ceb197012
codermarcos <marcosbmx.jr@gmail.com>
2018-01-12 07:56:18 -0200

    bug fix

--- themes/dev-dark-theme/scripts/index.js
+++ themes/dev-dark-theme/scripts/index.js
@@ -2,14 +2,14 @@ hexo.extend.helper.register('url_data', args => {
   let data = [];
 
   args.data.forEach(element => {
-    data.push(element.name.replace(new RegExp(' ', 'g'), '-').toLowerCase());
+    data.push(element.name.replace(new RegExp(' ', 'g'), '-'));
   });
 
   return data.join('/');
 });
 hexo.extend.helper.register('first_data', args => {
   let data = args.data[0] ? args.data[0].name : '';
-  return data.replace(new RegExp(' ', 'g'), '-').toLowerCase();
+  return data.replace(new RegExp(' ', 'g'), '-');
 });
 hexo.extend.helper.register('url_title', args => {
   return args.replace(new RegExp(' ', 'g'), '-');
